6f579cd7f5ad7e41fe84a4ca9d30429001087caf
Vova Vasiltsov <vova.vasiltsov.2017@mail.ru>
2024-01-15 16:27:37 +0500

    Refactoring the sidebar, made a model for the Relevance page, made the model display in the built-in Django admin panel. Created a superuser with the username and password 'root'.

--- projectDjango/main/admin.py
+++ projectDjango/main/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Relevance
 
 # Register your models here.
+class RelevanceDisplay(admin.ModelAdmin):
+    list_display = ('graph_salary_level', 'graph_num_vacancy', 'table')
+
+
+admin.site.register(Relevance, RelevanceDisplay)
+
--- projectDjango/main/models.py
+++ projectDjango/main/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+class ImageUpload(models.Model):
+    image = models.ImageField(upload_to='img', blank=False, verbose_name='Изображение')
+
+
+class Relevance(ImageUpload, models.Model):
+    graph_salary_level = models.ImageField(blank=False, verbose_name='График уровень зарплат по годам')
+    graph_num_vacancy = models.ImageField(blank=False, verbose_name='График количество вакансий по годам')
+    table = models.TextField(blank=False, verbose_name='Таблица')
+
+
